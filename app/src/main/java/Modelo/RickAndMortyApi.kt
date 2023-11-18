84ce957fadd3b2ed6c1f25259dcbcfa115c0f979
José Manuel Rodríguez Morales <z9manolo@gmail.com>
2023-11-18 10:23:34 -0600

    Se modifica clase principal

--- app/src/main/java/Modelo/RickAndMortyApi.kt
+++ app/src/main/java/Modelo/RickAndMortyApi.kt
@@ -17,9 +17,27 @@ class RickAndMortyApiService{
     }
 }
 
-data class RickAndMortyApi (
-    val Id: Int,
-    val Name: String,
-    val Species: String,
-    val Gender: Int
+data class RickAndMortyCharacter(
+    val id: Int,
+    val name: String,
+    val status: String,
+    val species: String,
+    val type: String,
+    val gender: String,
+    val origin: Origin,
+    val location: Location,
+    val image: String,
+    val episode: List<String>,
+    val url: String,
+    val created: String
+)
+
+data class Origin(
+    val name: String,
+    val url: String
+)
+
+data class Location(
+    val name: String,
+    val url: String
 )
